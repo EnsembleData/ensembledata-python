781d491c7ca9de664231163f7968bc0101292846
FaydSpeare <fspea123@gmail.com>
2024-08-06 20:17:15 +0200

    add error codes; avoid magic numbers

--- ensembledata/api/__init__.py
+++ ensembledata/api/__init__.py
@@ -1,6 +1,6 @@
 from ._async_client import EDAsyncClient
 from ._client import EDClient
-from ._errors import EDError
 from ._response import EDResponse
+from .errors import EDError
 
 __all__ = ["EDClient", "EDAsyncClient", "EDResponse", "EDError"]
