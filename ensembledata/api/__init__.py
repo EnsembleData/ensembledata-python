994c74d821fa46c4a370ab89d67babc09a487e47
FaydSpeare <fspea123@gmail.com>
2024-08-10 16:04:05 +0200

    dynamic version

--- ensembledata/api/__init__.py
+++ ensembledata/api/__init__.py
@@ -3,4 +3,6 @@ from ._client import EDClient
 from ._response import EDResponse
 from .errors import EDError
 
+__version__ = "0.1.3"
+
 __all__ = ["EDClient", "EDAsyncClient", "EDResponse", "EDError"]
