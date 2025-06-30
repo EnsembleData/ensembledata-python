3924853da3720291c8bde6014895c9f8da4d5f7e
fracogno <francesco.cognolato@gmail.com>
2025-06-30 18:03:07 +0200

    Updated

--- ensembledata/api/_async_client.py
+++ ensembledata/api/_async_client.py
@@ -846,25 +846,6 @@ class InstagramEndpoints:
         params = {k: v for k, v in params.items() if not (v is None or v is USE_DEFAULT)}
         return await self.requester.get("/instagram/post/details", params=params, timeout=timeout)
 
-    async def hashtag_posts(
-        self,
-        *,
-        hashtag: str,
-        cursor: str | UseDefault = USE_DEFAULT,
-        get_author_info: bool | UseDefault = USE_DEFAULT,
-        extra_params: Mapping[str, Any] | None = None,
-        timeout: float | None = None,
-    ) -> EDResponse:
-        params: dict[str, Any] = {
-            "name": hashtag,
-            "cursor": cursor,
-            "get_author_info": get_author_info,
-        }
-        if extra_params is not None:
-            params = {**extra_params, **params}
-        params = {k: v for k, v in params.items() if not (v is None or v is USE_DEFAULT)}
-        return await self.requester.get("/instagram/hashtag/posts", params=params, timeout=timeout)
-
     async def music_posts(
         self,
         *,
